59341539600d81df7a69055f868c4ffc239affc4
Pavel Bibichenko <b7086163@gmail.com>
2021-01-12 22:35:32 +0300

    Edit: add extensions note to ShaderTools.h

--- include/Shaders/ShaderTools.h
+++ include/Shaders/ShaderTools.h
@@ -7,6 +7,15 @@
 
 #include <string>
 
+/* File Extensions Note
+ *  .vert - a vertex shader
+    .tesc - a tessellation control shader
+    .tese - a tessellation evaluation shader
+    .geom - a geometry shader
+    .frag - a fragment shader
+    .comp - a compute shader
+ * */
+
 namespace eGLE
 {
     void checkError(int shader, int flag, const char *msg);
