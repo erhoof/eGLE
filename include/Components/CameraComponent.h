8aa74c4f912aa47c6e4c4a91220b213c1e299111
erhoof <tuxgU3gokwotxaxnas>
2021-01-12 15:18:25 +0300

    Add: Camera component

---
+++ include/Components/CameraComponent.h
@@ -0,0 +1,53 @@
+//
+// Created by erhoof on 12.01.2021.
+//
+
+#ifndef EGLE_CAMERACOMPONENT_H
+#define EGLE_CAMERACOMPONENT_H
+
+#include <glm/glm.hpp>
+
+#include "Component.h"
+
+namespace eGLE
+{
+    class RenderSystem;
+
+    class CameraComponent : Component {
+    public:
+        CameraComponent(float FOV, float aspectRatio, float zNear, float zFar);
+        ~CameraComponent() = default;
+
+        // RUI
+        void input(float timeDelta);
+
+        // RS
+        void updateRS(RenderSystem *rs);
+
+        // Getters / Setters
+        glm::mat4 viewMatrix();
+        glm::mat4 projectionMatrix();
+        glm::mat4 viewProjectionMatrix();
+
+    private:
+        // MVP
+        glm::mat4 m_viewMatrix;
+        glm::mat4 m_projectionMatrix;
+
+        // Camera Transform
+        glm::vec3 m_position;
+        glm::vec3 m_front;
+        glm::vec3 m_up;
+        glm::vec2 m_center;
+
+        // TODO: Camera Rotation (yaw, pitch, roll)
+
+        // Add-in Properties (sens: 0.005f)
+        float m_FOV;
+        float m_aspectRatio;
+        float m_zNear;
+        float m_zFar;
+    };
+}
+
+#endif //EGLE_CAMERACOMPONENT_H
---
+++ src/Components/CameraComponent.cpp
@@ -0,0 +1,55 @@
+//
+// Created by erhoof on 12.01.2021.
+//
+
+#include "../../include/Components/CameraComponent.h"
+#include "../../include/Core/Window.h"
+#include "../../include/Core/RenderSystem.h"
+
+#include <GLFW/glfw3.h>
+
+namespace eGLE
+{
+    CameraComponent::CameraComponent(float FOV, float aspectRatio, float zNear, float zFar) :
+        m_FOV(FOV),
+        m_aspectRatio(aspectRatio),
+        m_zNear(zNear),
+        m_zFar(zFar),
+        m_projectionMatrix(glm::perspective(FOV, aspectRatio, zNear, zFar))
+    {}
+
+    void CameraComponent::input(float timeDelta)
+    {
+        // example
+        if (glfwGetKey(Window::instance()->glfwWindow(), GLFW_KEY_W) == GLFW_PRESS)
+        {
+            transform().position().z += timeDelta;
+        }
+    }
+
+    void CameraComponent::updateRS(RenderSystem *rs)
+    {
+        rs->setMainCamera(this);
+    }
+
+    glm::mat4 CameraComponent::viewMatrix()
+    {
+        glm::vec3 position = transform().position();
+        glm::vec3 lookAt = transform().position() + transform().sideFront(); // to look forward
+        glm::vec3 top = transform().sideTop(); // top side
+
+        m_viewMatrix = glm::lookAt(position, target, top);
+
+        return m_viewMatrix;
+    }
+
+    glm::mat4 CameraComponent::projectionMatrix()
+    {
+        return m_projectionMatrix();
+    }
+
+    glm::mat4 CameraComponent::viewProjectionMatrix()
+    {
+        return m_projectionMatrix * viewMatrix();
+    }
+}
